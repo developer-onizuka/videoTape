68fff87a0e914da94952f67d697033ead8a326a6
developer-onizuka <67852921+developer-onizuka@users.noreply.github.com>
2022-04-12 18:49:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 from videotape import Videotape 
 
 vt = Videotape()
-print(vt.Greed())
+print(vt.Greet())
 print(vt.Name)
 print(vt.Position)
 print(vt.Forward(100))
@@ -12,7 +12,7 @@ print(vt.Backward(10))
 print("\n")
 
 vx = Videotape()
-print(vx.Greed())
+print(vx.Greet())
 print(vx.Name)
 print(vx.Position)
 print(vx.Forward(200))
